f21132f34bfe60b32b9120c2a98e97756ec89cf0
Phúc Đức <phucduc1402@gmail.com>
2025-12-02 18:27:44 +0700

    train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO('yolov8n-cls.pt')
 
 model.train(
-    data='dataset_raw/Images/Train',  # Đã có Train/Validate rồi
+    data='dataset_raw/Images/Train',  # Đã có Train/Validate
     epochs=15,
     imgsz=112,
     batch=64,
